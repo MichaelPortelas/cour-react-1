8ff538f4b0fc2d09b65deb82357e2c9592ad1896
Michael Portelas <michael.portelas@gmail.com>
2022-09-28 18:13:46 +0200

    init repo

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, modi facere! Excepturi ducimus at placeat assumenda aperiam harum enim corporis aliquid. Error iure ipsa minima, similique molestiae ut quod tempore beatae qui quisquam adipisci, assumenda amet! Recusandae consequuntur quo nulla. Totam quibusdam vero vel corrupti natus iure in beatae accusantium voluptas laborum. Incidunt, sunt nam? Voluptates, error saepe, fugit in distinctio quasi quae ab, id iste rem facere repudiandae. Quod placeat nihil tempora, ullam laborum illo ea consequuntur nesciunt magnam aperiam quisquam, vitae provident modi, doloremque molestiae! Recusandae tempore dolor sit itaque fugiat expedita. Quis porro nam dolorem! Obcaecati, ut.</p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, expedita maxime aut recusandae, vel esse, mollitia sequi sunt suscipit numquam id nisi quia aliquid dolores magnam. Adipisci ducimus at iure porro aspernatur iste libero sunt necessitatibus molestias minus sequi accusamus eaque atque nulla eum nesciunt dolorum nobis doloribus amet labore ipsam quidem nihil, veniam illo! Nisi voluptatem magni molestiae iure provident totam, vitae sunt voluptatum, et, illo id! Eaque maiores molestias nemo culpa ea voluptas mollitia quis aspernatur accusamus ullam numquam earum, velit, incidunt impedit quod omnis nihil, hic deleniti. Reprehenderit, ut dolore. Dolores aut eveniet asperiores maiores dolor ducimus sunt facere, error consequatur cupiditate possimus quia harum tempore nostrum suscipit dignissimos, repellat, labore id? Sunt magnam rem dolor, reiciendis aliquid eligendi ad ex, quisquam dolores adipisci fugiat temporibus. Sed nulla dolorem animi quas officiis doloribus perferendis illum illo voluptates dolores porro, repellat nihil, cum delectus omnis minima hic iusto nobis accusantium? Eius iusto atque iste similique, tempora facilis tenetur aspernatur incidunt vero deserunt. Impedit enim officia numquam possimus culpa, harum necessitatibus dolorem animi dicta repellendus minima similique blanditiis officiis voluptatem sit facilis tempora fuga reprehenderit temporibus recusandae ipsum aspernatur. Doloremque eum quo quasi, qui quod beatae dolor eligendi soluta!
+            </p>
+        </div>
+    );
+};
+
+export default About;
